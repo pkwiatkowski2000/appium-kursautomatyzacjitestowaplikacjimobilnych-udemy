9ba047c447bfc603ffb7b136c084af4311dff0bb
pkwiat <pawel.kwiatkowski2000@gmail.com>
2024-09-19 13:33:55 +0200

    19.09.2024

---
+++ src/main/java/org/example/SampleTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+    public static void main(String[] args) {
+        AndroidDriver driver;
+    }
+}
